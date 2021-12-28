60aa4e5b3a0c830f5a326cec735c38be136695a4
Chris Cleary <chris.cleary1993@gmail.com>
2021-12-28 19:33:05 +0000

    Add support for non-USD denominated stocks
    Previously if the user were to add a stock priced in e.g. GBP, the
    stock's value would simply be saved as if it were in USD.
    
    I have added checking stages to stock_check.py which verify that
    the stock is priced either in USD or a currency the exchange rate
    for which is stored in models.py. If the exchange rate is found
    the stock's value is converted to USD.

--- holdings/stock_check.py
+++ holdings/stock_check.py
@@ -1,5 +1,6 @@
 import environ
 import requests
+from holdings.models import Rate
 
 # Initialise environment variables
 env = environ.Env()
@@ -22,7 +23,12 @@ class StockCheck():
         response = requests.request("GET", self.yahoo_url, headers=headers, params=querystring)
         response_json = response.json()
         try:
-            stock_price = float(response_json['quoteResponse']['result'][0]['regularMarketPrice'])
+            if response_json['quoteResponse']['result'][0]['financialCurrency'] == 'USD':
+                stock_price = response_json['quoteResponse']['result'][0]['regularMarketPrice']
+            else:
+                for rate in Rate.objects.all():
+                    if response_json['quoteResponse']['result'][0]['financialCurrency'] == rate.name:
+                        stock_price = response_json['quoteResponse']['result'][0]['regularMarketPrice'] / float(rate.rate)
             return stock_price
         except:
             return False
@@ -39,6 +45,14 @@ class StockCheck():
         response_json = response.json()
         return_dict = {}
         for i in range(len(holdings)):
-            return_dict[holdings[i]] = response_json['quoteResponse']['result'][i]['regularMarketPrice']
+            if response_json['quoteResponse']['result'][i]['quoteType'] == 'EQUITY':
+                if response_json['quoteResponse']['result'][i]['financialCurrency'] == 'USD':
+                    return_dict[holdings[i]] = response_json['quoteResponse']['result'][i]['regularMarketPrice']
+                else:
+                    for rate in Rate.objects.all():
+                        if response_json['quoteResponse']['result'][i]['financialCurrency'] == rate.name:
+                            return_dict[holdings[i]] = response_json['quoteResponse']['result'][i]['regularMarketPrice'] / float(rate.rate)
+            else:
+                return_dict[holdings[i]] = response_json['quoteResponse']['result'][i]['regularMarketPrice']
         print(return_dict)
         return return_dict
